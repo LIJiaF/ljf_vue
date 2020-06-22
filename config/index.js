cac98d2ca2681b92489a2973889b96fbf6399b9f
ljf <2245849546>
2020-06-22 17:46:10 +0800

    修改vue配置文件

--- config/index.js
+++ config/index.js
@@ -11,11 +11,11 @@ module.exports = {
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {
-      '/api': {
+      '/api/**': {
         target: 'http://127.0.0.1:8888',
         changeOrigin: true,
         pathRewrite: {
-          '^/api': ''
+          '^/api': '/'
         }
       }
     },
